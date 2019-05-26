f9aec0f4cc3052e744893c3c6e27ffc0fcfd5872
Cagri Esen <esencgr@gmail.com>
2019-05-26 18:17:00 +0300

    add lib file

---
+++ basic_pointers.cpp
@@ -0,0 +1,22 @@
+// Using & and * operators..
+#include <iostream>
+using namespace std;
+int main(){
+    int a;       // a is an integer
+    int *a_ptr;  // a is a pointer to an integer.
+
+    a = 7;
+    a_ptr = &a;
+
+    cout << "adress of int a: " << &a << endl;
+    cout << "adress of variable a_ptr: " << a_ptr << endl << endl;
+
+    cout << "value of a: " << a << endl;
+    cout << "value of *a_ptr: " << *a_ptr << endl << endl;
+
+    cout << "& and * are conjugates.. "<< endl;
+    cout << "*&a_ptr: " << *&a_ptr << endl;
+    cout << "&*a_ptr: " << &*a_ptr << endl;
+
+    return 0;
+}
