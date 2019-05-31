c386f8de8e5fc9286e2503d83974d55c8b75b21e
Cagri Esen <esencgr@gmail.com>
2019-05-31 17:19:50 +0300

    modified

--- cube_function.cpp
+++ cube_function.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int cube_value ( int );
-void cube_reference ( int *, int * );
+int cube_reference ( int *, int * );
 
 int main (){
     int number = 5, number2 = 10, number3 = 100;
@@ -24,7 +24,7 @@ int cube_value ( int x ){              // call by value
     return x * x * x;                  // local "x" variable's cube
 }
 
-void cube_reference ( int *x_ptr, int *y_ptr){    // call by reference
+int cube_reference ( int *x_ptr, int *y_ptr){    // call by reference
     *x_ptr = (*x_ptr) * (*x_ptr) * (*x_ptr);      // "number2" variable's cube in main function
     *y_ptr = (*y_ptr) * (*y_ptr) * (*y_ptr);
 }
--- function_pointers.cpp
+++ function_pointers.cpp
@@ -3,29 +3,30 @@
 using namespace std;
 
 // scope of variable..
+int call_value ( int );
+int call_reference ( int *, int *, int * );
+
+int main (){
+
+    int p = 10, q = 20, r = 30;   // memory location is necessary for not getting of segmentation fault.
+
+    // this function means is basically that variables copy.
+    call_value ( p );             // this code return 10.
+    cout << p << endl;            // a and x are different variable and this ram memory adresses are different.
+
+    // it is possible that a lot of returned value in functions with call by reference.
+    call_reference ( &p, &q, &r );
+    cout << p << " " << q << " " << r << endl;
+
+    return 0;
+}
+
+// scope of variable..
 int call_value ( int x ){         // call by value
     x = 30;                       // this x value is only defined between of this brackets.
 }
 int call_reference ( int *x, int *y, int *z ){    // call by reference
-    *x = 30;
-    *y = 40;                      // this x, y, z value is defined when this pointers are called.
+    *x = *y * 30;
+    *y = *x * 40;                      // this x, y, z value is defined when this pointers are called.
     *z = 20;
-}
-
-int main (){
-
-    int a = 10, b = 20, c = 30;   // memory location is necessary for not getting of segmentation fault.
-    int *p = &a;
-    int *q = &b;
-    int *r = &c;
-
-    // this function means is basically that variables copy.
-    call_value ( a );             // this code return 10.
-    cout << a << endl;            // a and x are different variable and this ram memory adresses are different.
-
-    // it is possible that a lot of returned value in functions with call by reference.
-    call_reference ( p, q, r );
-    cout << *p << *q << *r << endl;
-
-    return 0;
 }
