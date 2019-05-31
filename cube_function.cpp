90e3f87ff1e4a4ce661cbeaf5fc914c25caed037
Cagri Esen <esencgr@gmail.com>
2019-05-31 15:41:50 +0300

    added code

--- cube_function.cpp
+++ cube_function.cpp
@@ -3,19 +3,19 @@
 using namespace std;
 
 int cube_value ( int );
-void cube_reference ( int * );
+void cube_reference ( int *, int * );
 
 int main (){
-    int number = 5;
+    int number = 5, number2 = 10, number3 = 100;
     cout << "The number is : " << number << endl;
 
-    cout << "Invocation by value : ";
+    cout << "Invocation by value ( 5^3 ): ";
     number = cube_value ( number );
     cout << number << endl;
 
-    cout << "Invocation by reference : ";
-    cube_reference ( &number );
-    cout << number << endl;
+    cout << "Invocation by reference ( 10^3 & 100^3 ): ";
+    cube_reference ( &number2, &number3 );
+    cout << number2 << " " << number3 << endl;
 
     return 0;
 }
@@ -24,6 +24,7 @@ int cube_value ( int x ){              // call by value
     return x * x * x;                  // local "x" variable's cube
 }
 
-void cube_reference ( int *x_ptr ){    // call by reference
-    (*x_ptr) * (*x_ptr) * (*x_ptr);    // "number" variable's cube in main function
+void cube_reference ( int *x_ptr, int *y_ptr){    // call by reference
+    *x_ptr = (*x_ptr) * (*x_ptr) * (*x_ptr);      // "number2" variable's cube in main function
+    *y_ptr = (*y_ptr) * (*y_ptr) * (*y_ptr);
 }
