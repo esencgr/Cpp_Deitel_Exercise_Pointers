b26af1d9519c132fb4582588a1c96c66f3b25938
Cagri Esen <esencgr@gmail.com>
2019-06-15 15:00:32 +0300

    add file

--- copy_the_string.cpp
+++ copy_the_string.cpp
@@ -14,20 +14,20 @@ int main(){
     const char string4[] = "CGR ESEN";
     char string3[ sizeof(string4) ];
 
-    copy1 ( string1, string2 );
+    copy1( string1, string2 );
     cout << "string1 : " << string1 << endl;
 
-    copy2 ( string3, string4 );
+    copy2( string3, string4 );
     cout << "string3 : " << string3;
 
     return 0;
 }
-// copying the s1 to s2 with array notation
+// copying the s1 to s2 with pointer notation
 void copy1( char *s1, const char *s2){
      for ( ; ( *s1 = *s2 ) != '\0'; s1++ ,s2++)
          ;
 }
-// copying the s1 to s2 with pointer notation
+// copying the s1 to s2 with array notation
 void copy2( char s1[], const char s2[] ){
      for ( int i = 0; (s1[ i ] = s2[ i ]) != '\0';  i++)
          ;
