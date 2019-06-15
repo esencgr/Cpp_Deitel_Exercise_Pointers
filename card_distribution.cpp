fbd2ad9f2af1901431541b93b8fabe3d6a033417
Cagri Esen <esencgr@gmail.com>
2019-06-15 20:09:04 +0300

    add file

--- card_distribution.cpp
+++ card_distribution.cpp
@@ -16,27 +16,27 @@ int main(){
     srand( time( 0 ) );
 
     mix( deck );
-    distribute ( deck, firs, sec );
+    distribute( deck, firs, sec );
 
     return 0;
 }
 
 void mix( int deck[ ][ 13 ] ){
     int row, col, card;
-    for (int card = 1; card <= 52; card++){
+    for ( int card = 1;  card <= 52;  card++ ){
         do{
            row = rand() % 4;
            col = rand() % 13;
-        }while( deck[ row ][ col ] != 0);
+        }while ( deck[ row ][ col ] != 0 );
         deck[ row ][ col ] = card;
     }
 }
 
 void distribute( const int deck[ ][ 13 ], const char *first[], const char *second[]){
-    for ( int card = 1;  card <= 52;  card++){
-        for ( int row = 0;  row < 4;  row++){
-            for ( int col = 0; col < 13;  col++){
-                if ( deck[ row ][ col ] == card ) {
+    for ( int card = 1;  card <= 52;  card++ ){
+        for ( int row = 0;  row < 4;  row++ ){
+            for ( int col = 0; col < 13;  col++ ){
+                if ( deck[ row ][ col ] == card ){
                     cout << first[ row ] << " " << second[ col ] << endl;
                 }
             }
