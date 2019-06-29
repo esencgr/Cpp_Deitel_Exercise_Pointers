82d4b4dc4c16b249886f5e8504c9c8914ce952e6
Cagri Esen <esencgr@gmail.com>
2019-06-30 02:22:35 +0300

    add straight function

--- card_distribution_modify.cpp
+++ card_distribution_modify.cpp
@@ -1,4 +1,4 @@
-/* Mix and distribute a 5-card deck.*/
+/* Mix and distribute a 10-card deck.*/
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
@@ -11,12 +11,13 @@ void pairs( const int [ ][ 13 ], const int [ ][ 2 ], const char *[] );
 void three_of_kind( const int [ ][ 13 ], const int [ ][ 2 ], const char *[] );
 void four_of_kind( const int [ ][ 13 ], const int [ ][ 2 ], const char *[] );
 void flush_hand( const int [ ][ 13 ], const int [ ][ 2 ], const char *[] );
+void straight( const int [ ][ 13 ], const int [ ][ 2 ], const char *[], const char *[] );
 
 int main(){
     const char *firs[ 4 ] = { "kupa", "karo", "sinek", "maca" };
     const char *sec[ 13 ] = { "as", "iki", "uc", "dort", "bes", "alti", "yedi",
                               "sekiz","dokuz","on","vale","kiz","papaz" };
-    int deck[ 4 ][ 13 ] = { 0 }, hand[ 5 ][ 2 ];
+    int deck[ 4 ][ 13 ] = { 0 }, hand[ 10 ][ 2 ];
 
     srand( time( 0 ) );
 
@@ -26,7 +27,7 @@ int main(){
     three_of_kind( deck, hand, sec );
     four_of_kind( deck, hand, sec );
     flush_hand( deck, hand, firs);
-
+    straight( deck, hand, firs, sec );
 
     return 0;
 }
@@ -45,7 +46,7 @@ void mix( int deck[ ][ 13 ] ){
 // deal a five card poker hand
 void distribute( const int deck[ ][ 13 ], const char *first[], const char *second[], int hand[ ][ 2 ]){
     int r = 0;
-    for ( int card = 1;  card <= 5;  card++ ){
+    for ( int card = 1;  card <= 10;  card++ ){
         for ( int row = 0;  row <= 3;  row++ ){
             for ( int col = 0; col <= 12;  col++ ){
                 if ( deck[ row ][ col ] == card ){
@@ -63,47 +64,75 @@ void distribute( const int deck[ ][ 13 ], const char *first[], const char *secon
 void pairs( const int deck[ ][ 13 ], const int hand[ ][ 2 ], const char *second[] ){
     int counter[ 13 ] = { 0 };
 
-    for ( int r = 0; r < 5; r++)
+    for ( int r = 0; r < 10; r++)
         ++counter[ hand [ r ][ 1 ] ];
 
     for ( int p = 0; p < 13; p++) {
         if ( counter[ p ] == 2 )
-           cout << "\nThe hand contains a pair of " << second[ p ] << endl;
+            cout << "\nThe hand contains pair  " << second[ p ] << endl;
     }
 }
 
 void three_of_kind( const int deck[ ][ 13 ], const int hand[ ][ 2 ], const char *second[] ){
     int counter[ 13 ] = { 0 };
 
-    for ( int r = 0; r < 5; r++)
+    for ( int r = 0; r < 10; r++)
         ++counter[ hand [ r ][ 1 ] ];
 
     for ( int p = 0; p < 13; p++) {
         if ( counter[ p ] == 3 )
-           cout << "\nThe hand contains three " << second[ p ] << endl;
+            cout << "\nThe hand contains three " << second[ p ] << endl;
     }
 }
 
 void four_of_kind( const int deck[ ][ 13 ], const int hand[ ][ 2 ], const char *second[] ){
     int counter[ 13 ] = { 0 };
 
-    for ( int r = 0; r < 5; r++)
+    for ( int r = 0; r < 10; r++)
         ++counter[ hand [ r ][ 1 ] ];
 
     for ( int p = 0; p < 13; p++) {
         if ( counter[ p ] == 4 )
-           cout << "\nThe hand contains four " << second[ p ] << endl;
+            cout << "\nThe hand contains four " << second[ p ] << endl;
     }
 }
 
 void flush_hand( const int deck[ ][ 13 ], const int hand[ ][ 2 ], const char *first[] ){
     int counter[ 4 ] = { 0 };
 
-    for ( int r = 0; r < 5; r++)
+    for ( int r = 0; r < 10; r++)
         ++counter[ hand [ r ][ 0 ] ];
 
-    for ( int p = 0; p < 5; p++) {
+    for ( int p = 0; p < 10; p++){
         if ( counter[ p ] == 5 )
-           cout << "\nThe hand contains flush " << first[ p ] << endl;
+            cout << "\nThe hand contains flush " << first[ p ] << endl;
     }
+}
+
+void straight( const int deck[ ][ 13 ], const int hand[ ][ 2 ], const char *first[], const char *second[] ){
+     int s[ 10 ] = { 0 }, temp;
+
+     // copy column locations to sort
+     for ( int r = 0; r < 10; r++)
+         s[ r ] = hand[ r ][ 1 ];
+
+     // bubble sort column locations
+     for ( int pass = 1; pass < 10; pass++ ){
+         for ( int comp = 0; comp < 4; comp++ ){
+             if ( s[ comp ] > s[ comp + 1 ] ){
+                 temp = s[ comp ];
+                 s[ comp ] = s[ comp + 1 ];
+                 s[ comp + 1 ] = temp;
+              }
+         }
+     }
+
+     // check if sorted columns are a straight
+     if ( s[ 10 ] - 1 == s[ 9 ] && s[ 9 ] - 1 == s[ 8 ] && s[ 8 ] - 1 == s[ 7 ] && s[ 7 ] - 1 == s[ 6 ] &&
+          s[ 6 ] - 1 == s[ 5 ] && s[ 5 ] - 1 == s[ 4 ] && s[ 4 ] - 1 == s[ 3 ] && s[ 3 ] - 1 == s[ 2 ] &&
+          s[ 2 ] - 1 == s[ 1 ] && s[ 1 ] - 1 == s[ 0 ] ){
+         cout << "\n The hand contains a straight consisting of\n";
+         for ( int j = 0; j < 10; ++j )
+             cout << second[ hand[ j ][ 1 ] ] << " of " << first[ hand[ j ][ 0 ] ] << '\n';
+     }
 }
